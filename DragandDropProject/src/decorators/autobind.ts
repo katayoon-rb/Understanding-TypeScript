032504f885b4fe72504ce94613dfae2691268f16
katayoon-rb <55879886+katayoon-rb@users.noreply.github.com>
2023-12-03 21:05:12 +0330

    Add files via upload

---
+++ DragandDropProject/src/decorators/autobind.ts
@@ -0,0 +1,11 @@
+export function autobind(
+  _: any,
+  _2: string,
+  descriptor: PropertyDescriptor
+) {
+  const adjDescriptor: PropertyDescriptor = {
+    configurable: true,
+    get() { return descriptor.value.bind(this) }
+  }
+  return adjDescriptor
+}
